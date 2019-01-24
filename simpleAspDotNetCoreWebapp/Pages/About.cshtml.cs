2efb60305a38fe0d164058ad71ba40a1ce7b0692
David Coombes <davidc@contentmaster.com>
2019-01-24 10:25:25 +0000

    Initial upload

---
+++ simpleAspDotNetCoreWebapp/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace simpleAspDotNetCoreWebapp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        System.Diagnostics.Trace.WriteLine("Verbose message, in the OnGet method for About.cshtml");
+        System.Diagnostics.Trace.TraceError("Error message, in the OnGet method for About.cshtml");
+        System.Diagnostics.Trace.TraceWarning("Warning  message, in the OnGet method for About.cshtml");
+        System.Diagnostics.Trace.TraceInformation("Information message, in the OnGet method for About.cshtml");
+        }
+    }
+}
---
+++ simpleAspDotNetCoreWebapp/Pages/Contact.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace simpleAspDotNetCoreWebapp.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        System.Diagnostics.Trace.WriteLine("Verbose message, in the OnGet method for Contact.cshtml");
+        System.Diagnostics.Trace.TraceError("Error message, in the OnGet method for Contact.cshtml");
+        System.Diagnostics.Trace.TraceWarning("Warning  message, in the OnGet method for Contact.cshtml");
+        System.Diagnostics.Trace.TraceInformation("Information message, in the OnGet method for Contact.cshtml");
+        }
+    }
+}
